6ce04a28e3151b3fd792f26a7572c49a78c5c626
unknown <1084857417@qq.com>
2021-01-26 21:03:59 +0800

    new

---
+++ miniprogram/pages/music/music.js
@@ -0,0 +1,135 @@
+// pages/music/music.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    imgUrls:[
+      {
+        url:'http://p1.music.126.net/Z90NF2dHuBYrV6x-U9jJJQ==/109951165664719544.jpg?imageView&quality=89'
+      },
+      {
+        url:'http://p1.music.126.net/nVUH7O5ZNMG1OQ1kE-tq9g==/109951165665595417.jpg?imageView&quality=89'
+      },
+      {
+        url:'http://p1.music.126.net/vAjwukVm-H0LOqzy4FTJXA==/109951165664851877.jpg?imageView&quality=89'
+      },
+      {
+        url:'http://p1.music.126.net/j0gp3gBDRRoqIXxAs0v7oA==/109951165664720877.jpg?imageView&quality=89'
+      },
+      {
+        url:'http://p1.music.126.net/9Ayx-EeCnuLRWKTcIhGB6g==/109951165664742856.jpg?imageView&quality=89'
+      },
+      {
+        url:'http://p1.music.126.net/C9I9GxpvRX7nCZyXNBeqOw==/109951165664694558.jpg?imageView&quality=89'
+      },
+      {
+        url:'http://p1.music.126.net/q5rKcBx9Y0V37DsUSaQKXg==/109951165664695730.jpg?imageView&quality=89'
+      },
+      {
+        url:'http://p1.music.126.net/WOoIZuva_umxxzYOvWINLA==/109951165664707565.jpg?imageView&quality=89'
+      },
+      {
+        url:'http://p1.music.126.net/pOXTFta-mhTpZOGhBBWvhQ==/109951165664682857.jpg?imageView&quality=89'
+      },
+      {
+        url:'http://p1.music.126.net/UdSM2BmqY_h_t9HAOzb5dQ==/109951165664710664.jpg?imageView&quality=89'
+      },
+    ],
+    playlist:[
+      {
+        "id":"0001",
+        "playCount":1.4641238e+06,
+        "name":"Acoustic/沉浸柔情海洋",
+        "picUrl":"http://p3.music.126.net/6usq3fdV3_Ldu_3dwqz9yQ==/109951165595569687.jpg?param=200y200"
+      },
+      {
+        "id":"0002",
+        "playCount":622822.6,
+        "name":"做我的公主吧 我把皇冠给你",
+        "picUrl":"http://p4.music.126.net/ZJJ1ZKZ9_myIdOMKqT2LAQ==/109951165426759607.jpg?param=140y140"
+      },
+      {
+        "id":"0003",
+        "playCount":1.785097e+06,
+        "name":"皮皮虾渡夕（BGM）",
+        "picUrl":"http://p3.music.126.net/Y4yCHC_COlb4P1upACm4cQ==/109951165532510984.jpg?param=200y200"
+      },
+      {
+        "id":"0004",
+        "playCount":1.886998e+06,
+        "name":"打游戏必备神曲♂",
+        "picUrl":"http://p4.music.126.net/e1tc21Nl7x3PVw7gUqsXjA==/109951165468135579.jpg?param=140y140"
+      },
+      {
+        "id":"0005",
+        "playCount":7.874499e+06,
+        "name":"//今日份的全糖去冰//情话Rap；情歌说唱",
+        "picUrl":"http://p3.music.126.net/b20UjBbD0ggEwO3Ylq2pMw==/109951165482242795.jpg?param=140y140"
+      },
+      {
+        "id":"0006",
+        "playCount":666.676677e+06,
+        "name":"英",
+        "picUrl":"http://p3.music.126.net/KGc09_iZqqRmSULoXQzgxg==/109951163814830945.jpg?param=200y200"
+      },
+    ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
